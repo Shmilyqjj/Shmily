4dea11afcef453bd5b1ceb852372d866843f1067
Shmilyqjj <710552907@qq.com>
2018-11-04 14:21:02 +0800

    Test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.print("Shmily");
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();//可以读入带空格的字符串
+        System.out.println(s);
+        String s0 = in.next();//遇到空格停止
+        System.out.println(s0);
+        }
     }
-}
+
