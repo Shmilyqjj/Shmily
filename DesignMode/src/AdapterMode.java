89af39ac5f7ca53250e67a8b656dbc383eac4a3d
Shmilyqjj <710552907@qq.com>
2018-11-25 11:52:39 +0800

    Test

--- DesignMode/src/AdapterMode.java
+++ DesignMode/src/AdapterMode.java
@@ -43,3 +43,38 @@ class PowerBImpl implements PowerB{
         System.out.println("PowerB工作");
     }
 }
+
+
+
+
+
+
+
+
+interface IPerson{
+    public void eat();
+    public void swim();
+    public void run();
+    public void sing();
+}
+abstract class Adp implements IPerson{
+    public void eat(){}
+    public void swim(){}
+    public void run(){}
+    public void sing(){}
+}
+
+class Man extends Adp{
+    public void sing(){
+
+    }
+    public void run(){}
+}
+class Woman implements IPerson{
+    public void eat(){}
+    public void swim(){
+
+    }
+    public void run(){}
+    public void sing(){}
+}
