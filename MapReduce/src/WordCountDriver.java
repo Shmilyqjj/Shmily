abbb51273d68cce7c10f8c3b3aee25ee69c20a38
Shmilyqjj <710552907@qq.com>
2019-01-16 12:05:22 +0800

    MapReduce.jar 编译

---
+++ MapReduce/src/WordCountDriver.java
@@ -0,0 +1,36 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class WordCountDriver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        //1 获取配置信息，或者job对象实例
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration);
+        // 2 设置指定本程序的jar包所在的本地路径
+        job.setJarByClass(WordCountDriver.class);
+        //3 设置map 和 reducer的类   指定本业务job要使用的mapper/Reducer业务类
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        //4 设置 map的输出类型 key - value类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        //5 设置 reducer的输出类型 key - value类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        //6 设置输入输出路径 指定job的输入原始文件所在目录
+
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        //7 将job中配置的相关参数，以及job所用的java类所在的jar包， 提交给yarn去运行
+        boolean result = job.waitForCompletion(true);
+        System.exit(result?0:1);
+
+    }
+}
