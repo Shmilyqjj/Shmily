e1d6aa4c210f9334ec13600e00eeca7d970e9491
Shmilyqjj <710552907@qq.com>
2019-05-17 20:09:33 +0800

    test

---
+++ JianZhiOffer_Problems/src/Problem3.java
@@ -0,0 +1,24 @@
+/**
+ * 输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。
+ */
+import java.util.ArrayList;
+public class Problem3 {
+
+}
+/**
+ *    public class ListNode {
+ *        int val;
+ *        ListNode next = null;
+ *
+ *        ListNode(int val) {
+ *            this.val = val;
+ *        }
+ *    }
+ *
+ */
+
+class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+
+    }
+}
