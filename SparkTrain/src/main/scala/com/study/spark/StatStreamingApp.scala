f1597fad53bbb6ce1aae3dc43b9949f0efd67f0b
Shmilyqjj <710552907@qq.com>
2019-08-01 09:21:08 +0800

    test

---
+++ SparkTrain/src/main/scala/com/study/spark/StatStreamingApp.scala
@@ -0,0 +1,12 @@
+package com.study.spark
+
+/**
+  * 爱奇艺视频网站大数据Spark项目实战 连接kafka和SparkStreaming部分 使用SparkStreaming
+  */
+
+object StatStreamingApp {
+  def main(args: Array[String]): Unit = {
+    //    println("My-AiQiYi-Data-SparkStreaming-Project")
+
+  }
+}
