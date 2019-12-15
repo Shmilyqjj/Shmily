23ae560390407d0579537d23d24de87ad89f4624
Shmilyqjj <710552907@qq.com>
2019-12-16 01:05:16 +0800

    Scala语言学习

---
+++ Spark/src/main/scala/scala/ScalaTest.scala
@@ -0,0 +1,13 @@
+package scala
+
+/**
+  * @Description: Scala语言学习
+  * @Owner: jiajing_qu
+  * @Date: 2019/12/16 0:55
+  * @Version: 1.0
+  */
+object ScalaTest {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala")
+  }
+}
