bc23a0455359269f000b200a07b3cb340d77dd5e
Shmilyqjj <710552907@qq.com>
2020-03-12 10:55:06 +0800

    test

--- CollectionTest/src/setTest.java
+++ CollectionTest/src/setTest.java
@@ -5,7 +5,6 @@ import java.util.Set;
  * set 一个不包含重复元素的 collection。更确切地讲，set 不包含满足 e1.equals(e2) 的元素对 e1 和 e2，并且最多包含一个 null 元素。
  *
  *
- *
  */
 public class  setTest {
     public static void main(String[] args) {
