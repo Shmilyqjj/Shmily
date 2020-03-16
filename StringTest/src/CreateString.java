860648708989d821d98e2650ad3bd14c4e4a486e
Shmilyqjj <710552907@qq.com>
2020-03-16 15:42:09 +0800

    String创建 对比 和存储 说明

---
+++ StringTest/src/CreateString.java
@@ -0,0 +1,36 @@
+/**
+ * 创建String 存放位置 以及 intern方法
+ */
+public class CreateString {
+    public static void main(String[] args) {
+        String s1 = "qjj";  //先去JVM常量池找是否已经有“qjj” 如果没有则存入JVM常量池
+        String s2 = new String("qjj");  //创建String对象 到堆区
+        final String s3 = "qjj";  //先去JVM常量池找是否已经有“qjj” 如果没有则存入JVM常量池 已经有了则指向那个地址
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));  //equals判断值相等
+        System.out.println(s1 == s3);   //指向同一个地址  两个string对象相等
+
+        s2.intern();  //调用intern 会把字符串保存到常量池中
+
+        /**
+         * JDK 1.7之后，永久代 改为 元空间
+         * 字符串常量池从方法区移到堆区
+         */
+
+        String s4 = "q" + "jj";  // 编译器优化了string拼接
+        System.out.println(s1 == s4);  //True
+
+
+        /**
+         * 字符串对比：
+         * equals对比String的值是否相等
+         * == 比较的是字符串对象
+         * compareTo是对字符串中的每个字符进行比较 ，不同的就返回差值（即返回int类型）
+         */
+        String s5 = "qjj";
+        String s6 = "gjj";
+        System.out.println(s5.compareTo(s6));
+
+    }
+}
