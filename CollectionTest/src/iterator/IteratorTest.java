c48a919b6985a4753990e3ccea6c8de183aa7c93
Shmilyqjj <710552907@qq.com>
2020-03-20 08:48:52 +0800

    Iterator 复习

--- CollectionTest/src/iterator/IteratorTest.java
+++ CollectionTest/src/iterator/IteratorTest.java
@@ -7,8 +7,11 @@ import java.util.ListIterator;
 
 /**
  * Iterator更安全，当前遍历的集合被修改时会报异常
+ *Iterator单向遍历，ListIterator可以双向遍历 ListIterator继承了Iterator，有更多功能
  *
- * Iterator单向遍历，ListIterator可以双向遍历 ListIterator继承了Iterator，有更多功能
+ * 首先要明白Iterator和for遍历的优缺点
+ * Iterator迭代器遍历方式，适用于连续内存存储方式(数组、 ArrayList（其实 ArrayList底层实现也是数组形式）),缺点是只能从头开始遍历，优点是可以边遍历边删除。
+ * for遍历灵活，可以在指定位置开始遍历，但不能边遍历边删除，否则抛异常。
  */
 public class IteratorTest {
     public static void main(String[] args) {
@@ -31,11 +34,11 @@ public class IteratorTest {
             System.out.print(lit.next());  //hasPrevious向后遍历 ListIterator独有
         }
         lit.add("love");
-        lit.add("zz");
+        lit.add("abcd");
 
         System.out.println();
 
-        for (int i = 0; i < ls.size(); i++) {
+        for(int i = 0; i < ls.size(); i++) {
             System.out.print(ls.get(i));
         }
 
