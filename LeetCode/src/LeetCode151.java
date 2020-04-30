7cb0c40da9193a0bdfc73071a223f4ec13006c14
Shmilyqjj <710552907@qq.com>
2020-04-30 15:56:27 +0800

    modify

---
+++ LeetCode/src/LeetCode151.java
@@ -0,0 +1,20 @@
+/**
+ * Description: LeetCode 151.翻转字符串里的单词
+ * Date: 2020/4/30 13:03
+ * Author: 佳境Shmily
+ * Site: shmily-qjj.top
+ *
+ * 题目链接：https://leetcode-cn.com/problems/reverse-words-in-a-string/
+ */
+public class LeetCode151 {
+    public static void main(String[] args) {
+        LeetCode151 solution = new LeetCode151();
+        System.out.println(solution.reverseWords("  hello world!  "));
+        System.out.println(solution.reverseWords("a good   example"));
+    }
+
+    public String reverseWords(String s) {
+        // 逻辑部分
+        return s;
+    }
+}
