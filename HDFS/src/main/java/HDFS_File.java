87473c5e3a3babcfd742cdf5d7bf8ee231a0b579
Shmilyqjj <710552907@qq.com>
2020-04-30 16:05:53 +0800

    HDFS学习

---
+++ HDFS/src/main/java/HDFS_File.java
@@ -0,0 +1,32 @@
+package main.java;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class HDFS_File{
+    public static void main(String[] args) throws Exception, IllegalArgumentException, IOException  {
+        // 1 创建配置信息对象
+        Configuration configuration = new Configuration();
+
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop101:9000"),configuration, "root");
+
+        // 2 获取查询路径下的文件状态信息
+        FileStatus[] listStatus = fs.listStatus(new Path("/"));
+
+        // 3 遍历所有文件状态
+        for (FileStatus status : listStatus) {
+            if (status.isFile()) {
+                System.out.println("f--" + status.getPath().getName());
+            } else {
+                System.out.println("d--" + status.getPath().getName());
+            }
+        }
+    }
+}
+
+
---
+++ HDFS/src/main/java/MyDNSToSwitchMapping.java
@@ -0,0 +1,38 @@
+package main.java;
+
+import org.apache.hadoop.net.DNSToSwitchMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyDNSToSwitchMapping implements DNSToSwitchMapping{
+    // 传递的是客户端的ip列表，返回机架感知的路径列表
+    public List<String> resolve(List<String> names) {
+        ArrayList<String> lists = new ArrayList<String>();
+        if (names != null && names.size() > 0) {
+            for (String name : names) {
+                int ip = 0;
+                //1 获取ip地址
+                if (name.startsWith("hadoop")) {
+                    String no = name.substring(6);
+                    // hadoop102
+                    ip = Integer.parseInt(no);
+                } else if (name.startsWith("192")) {
+                    // 192.168.1.102
+                    ip = Integer.parseInt(name.substring(name.lastIndexOf(".") + 1));
+                }
+                //2 定义机架
+                if (ip < 104) {
+                    lists.add("/rack1/" + ip);
+                } else {
+                    lists.add("/rack2/" + ip);
+                }
+            }
+        }
+        return lists;
+    }
+    public void reloadCachedMappings() {
+    }
+    public void reloadCachedMappings(List<String> names) {
+    }
+}
