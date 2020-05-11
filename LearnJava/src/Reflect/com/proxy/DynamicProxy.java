d758ab56bb149c125550c28eac078b2d8ac53e5f
Shmilyqjj <710552907@qq.com>
2020-05-11 12:59:56 +0800

    java Reflect

---
+++ LearnJava/src/Reflect/com/proxy/DynamicProxy.java
@@ -0,0 +1,14 @@
+package com.proxy;
+
+/**
+ * 动态代理
+ * 动态代理是通过代理类Proxy  接口和实现类之间可以不直接发生关系，而可以在运行期实现动态关联
+ */
+public class DynamicProxy {
+    public static void main(String[] args) {
+        Person person = new Person();//买东西
+        ProxyBuy pb = new ProxyBuy();  //代理
+        IShopping shopping = (IShopping)pb.create(person); //shopping代理对象
+        shopping.buy(); //直接调用被代理类的方法
+    }
+}
