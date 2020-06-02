688716953ba9747a4fd83608841e6633e7e7c784
Shmilyqjj <710552907@qq.com>
2020-06-02 12:39:22 +0800

    rm-files

---
+++ Scala/src/main/scala/base/circulation/ForAndWhile.scala
@@ -0,0 +1,13 @@
+package base.circulation
+
+/**
+  * Description: Scala for循环 while循环
+  * Date: 2020/5/24 13:06
+  * Author: 佳境Shmily
+  * Site: shmily-qjj.top
+  */
+object ForAndWhile {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
