5b8668ac98e50d9d01c2ef53bf0bc75e5321a3c2
Shmilyqjj <710552907@qq.com>
2020-06-25 10:14:31 +0800

    Scala match case

--- Scala/src/main/scala/base/functions/scalaFunctions.scala
+++ Scala/src/main/scala/base/functions/scalaFunctions.scala
@@ -19,6 +19,13 @@ object scalaFunctions {
     val closure = (i:Int) => i * x
     println(closure(1))
     println(closure(2))
+
+    println("**********************")
+    println(op("+", 1, 2))
+    println(op("-", 1, 2))
+    println(op("*", 1, 2))
+    println(op("/", 1, 2))
+    println(op1("/", 1, 2))
   }
 
   def addValue(args: Int*): Int = {  //普通函数  多参数函数
@@ -27,6 +34,24 @@ object scalaFunctions {
       i += arg
     }
     i
+  }
+
+  def op(x:String,a:Int,b:Int): Any ={
+    var result:Any = 0
+    x match {
+      case "+" => result = a + b
+      case "-" => result = a - b
+      case "*" => result = a * b
+      case "/" => result = a * 1.0 / b
+    }
+    result
+  }
+
+  def op1(x:String,a:Int,b:Int):Any = x match {
+    case "+" => a + b
+    case "-" => a - b
+    case "*" => a * b
+    case "/" => a * 1.0 / b
   }
 
 }
