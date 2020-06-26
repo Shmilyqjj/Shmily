d5d4378bb0cd38aef6935d751abea58490b9ad1d
Shmilyqjj <710552907@qq.com>
2020-06-26 11:02:49 +0800

    Scala方法

--- Scala/src/main/scala/base/functions/scalaFunctions.scala
+++ Scala/src/main/scala/base/functions/scalaFunctions.scala
@@ -20,12 +20,6 @@ object scalaFunctions {
     println(closure(1))
     println(closure(2))
 
-    println("**********************")
-    println(op("+", 1, 2))
-    println(op("-", 1, 2))
-    println(op("*", 1, 2))
-    println(op("/", 1, 2))
-    println(op1("/", 1, 2))
   }
 
   def addValue(args: Int*): Int = {  //普通函数  多参数函数
@@ -36,22 +30,5 @@ object scalaFunctions {
     i
   }
 
-  def op(x:String,a:Int,b:Int): Any ={
-    var result:Any = 0
-    x match {
-      case "+" => result = a + b
-      case "-" => result = a - b
-      case "*" => result = a * b
-      case "/" => result = a * 1.0 / b
-    }
-    result
-  }
-
-  def op1(x:String,a:Int,b:Int):Any = x match {
-    case "+" => a + b
-    case "-" => a - b
-    case "*" => a * b
-    case "/" => a * 1.0 / b
-  }
 
 }
