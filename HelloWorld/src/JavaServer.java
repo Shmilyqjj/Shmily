ee4e81ed17d49ff5b933d86796796f87c270d879
Shmilyqjj <710552907@qq.com>
2020-07-22 23:00:43 +0800

    py4j JavaServer

---
+++ HelloWorld/src/JavaServer.java
@@ -0,0 +1,18 @@
+import py4j.GatewayServer;
+public class JavaServer {
+
+  public static void main(String[] args) {
+    JavaClass javaClass = new JavaClass();    // app is now the gateway.entry_point
+    GatewayServer server = new GatewayServer(javaClass);
+    //GatewayServer server = new GatewayServer(app,25334); //Use other port -- 25333
+    server.start(); //receive python requests
+  }
+
+
+}
+
+class JavaClass{
+  public int javaMethod(int first, int second) {
+    return first + second;
+  }
+}
