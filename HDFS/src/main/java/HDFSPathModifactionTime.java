1ff8d76d0311c49d0fe87caf62115df4840ef190
Shmilyqjj <710552907@qq.com>
2020-09-13 23:04:10 +0800

    HDFS API

---
+++ HDFS/src/main/java/HDFSPathModifactionTime.java
@@ -0,0 +1,45 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * :Description: 查看HDFS目录更新时间
+ * :Author: 佳境Shmily
+ * :Create Time: 2020/9/13 22:57
+ * :Site: shmily-qjj.top
+ */
+public class HDFSPathModifactionTime {
+    public static void main(String[] args) throws IOException {
+
+        Configuration conf = new Configuration();
+        conf.addResource(new Path("/etc/hadoop/conf/core-site.xml"));
+        conf.addResource(new Path("/etc/hadoop/conf/hdfs-site.xml"));
+        FileSystem fs = FileSystem.get(conf);
+        System.out.println("输入目录名称:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Path path = new Path(br.readLine());
+        displayDirectoryContents(fs, path);
+        fs.close();
+    }
+
+    private static void displayDirectoryContents(FileSystem fs, Path rootDir) {
+        try {
+            FileStatus[] status = fs.listStatus(rootDir);
+            for (FileStatus file : status) {
+                if (file.isDir()) {
+                    System.out.println("DIRECTORY:" + file.getPath() + " - 上次修改time:" + file.getModificationTime());
+                    displayDirectoryContents(fs, file.getPath());
+                } else {
+                    System.out.println("FILE:" + file.getPath() + " - 最后修改时间:" + file.getModificationTime());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- HDFS/src/main/java/HDFS_Download.java
+++ HDFS/src/main/java/HDFS_Download.java
@@ -2,7 +2,6 @@
  * HDFS文件上传
  */
 
-import org.apache.commons.httpclient.URIException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -12,7 +11,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class HDFS_Download {
-    public static void main(String[] args) throws IOException, URIException, URISyntaxException, InterruptedException {
+    public static void main(String[] args) throws IOException,URISyntaxException, InterruptedException {
         Configuration configuration = new Configuration();
         FileSystem filesystem = FileSystem.get(new URI("hdfs://hadoop101:8020"), configuration, "root");
         filesystem.copyToLocalFile(new Path("/media/xiyou.txt"),new Path("C:\\Users\\Home-PC\\Desktop\\xiyou.txt"));
