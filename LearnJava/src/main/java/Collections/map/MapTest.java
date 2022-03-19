b704660d41d52442c2deea41c1ce97cf6f2e7485
shmily <710552907@qq.com>
2022-03-19 22:33:01 +0800

    stream update

--- LearnJava/src/main/java/Collections/map/MapTest.java
+++ LearnJava/src/main/java/Collections/map/MapTest.java
@@ -14,6 +14,13 @@ public class MapTest {
         System.out.println(map.containsKey(4));
         System.out.println(map.containsValue("q"));
         System.out.println(map.get(2));
+        map.entrySet().stream().forEach(System.out::println);
+        System.out.println(map.toString());
+
+        Iterator iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
 
         System.out.println("**********************************");
         Map map1 = new Hashtable();  // Hashtable实现了MAP(K,V)
@@ -21,5 +28,9 @@ public class MapTest {
         map1.put(2, "j");
         map1.put(3, "j");
         System.out.println(map1.toString());
+
+        System.out.println(Integer.valueOf("2134"));
+        System.out.println("dadwdp_wx".replaceFirst("^p_", ""));
+
     }
 }
