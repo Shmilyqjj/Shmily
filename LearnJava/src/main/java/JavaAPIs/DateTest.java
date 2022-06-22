19b79b35c548cdd435c14eb03adec3768ba329b2
shmilyqjj <710552907@qq.com>
2022-06-22 16:58:55 +0800

    add timezone ts format method

--- LearnJava/src/main/java/JavaAPIs/DateTest.java
+++ LearnJava/src/main/java/JavaAPIs/DateTest.java
@@ -1,10 +1,12 @@
 package JavaAPIs;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 /**
  * 功能：日期显示 !!!
@@ -12,7 +14,7 @@ import java.util.GregorianCalendar;
  */
 
 public class DateTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Date date = new Date(); //java.util
         System.out.println(date);//显示日期
 
@@ -39,6 +41,39 @@ public class DateTest {
         DateFormat df1 = new SimpleDateFormat("yyyy年MM月dd日 hh:mm:ss S"); //时间格式化-按模版  - 字母表示查手册
         System.out.println(df1.format(date));  //传入Date的对象，格式变化
 
+
+        System.out.println("######################################################################");
+        printAvailableTimezone();
+        String dateStr = timeConvertWithTimezone(System.currentTimeMillis(), "Asia/Shanghai", " Asia/Tokyo ", "yyyy-MM-dd HH:mm:ss.SSS");
+        System.out.println(dateStr);
+
+
+
+    }
+
+    /**
+     * 带时区的时间转换format
+     * @param ts 时间戳
+     * @param sourceTimeZone 源时区
+     * @param targetTimeZone 转换输出的目标时区
+     * @param format SimpleDateFormat的参数 指定格式
+     * @return
+     */
+    public static String timeConvertWithTimezone(long ts, String sourceTimeZone, String targetTimeZone, String format){
+        TimeZone.setDefault(TimeZone.getTimeZone(sourceTimeZone.trim()));
+        DateFormat df = new SimpleDateFormat(format);
+        df.setTimeZone(TimeZone.getTimeZone(targetTimeZone.trim()));
+        return df.format(ts);
+    }
+
+    /**
+     * 输出可用时区
+     */
+    public static void printAvailableTimezone(){
+        String[] ids = TimeZone.getAvailableIDs();
+        for (String id:ids) {
+            System.out.println(id);
+        }
     }
 }
 
