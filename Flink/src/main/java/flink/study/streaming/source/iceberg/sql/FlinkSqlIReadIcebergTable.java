addb9cb00c363cec1937093121056cffca955388
shmily <710552907@qq.com>
2022-10-06 20:57:28 +0800

    Iceberg

---
+++ Flink/src/main/java/flink/study/streaming/source/iceberg/sql/FlinkSqlIReadIcebergTable.java
@@ -0,0 +1,46 @@
+package flink.study.streaming.source.iceberg.sql;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.TableResult;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+/**
+ * @author 佳境Shmily
+ * @Description:
+ * @CreateTime: 2022/10/6 18:16
+ * @Site: shmily-qjj.top
+ * 运行环境变量HADOOP_CONF_DIR=E:\CDH-Conf\
+ */
+public class FlinkSqlIReadIcebergTable {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+        env.enableCheckpointing(5000L);
+
+        // 配置table.dynamic-table-options.enabl=true 使sql可以使用Hint
+        Configuration configuration = tEnv.getConfig().getConfiguration();
+        configuration.setBoolean("table.dynamic-table-options.enable", true);
+
+        tEnv.executeSql("CREATE CATALOG hadoop_iceberg_catalog WITH (\n" +
+                "  'type'='iceberg',\n" +
+                "  'catalog-type'='hadoop',\n" +
+                "  'warehouse'='hdfs://nameservice/user/iceberg/warehouse',\n" +
+                "  'property-version'='1'\n" +
+                ")");
+
+        tEnv.useCatalog("hadoop_iceberg_catalog");
+        tEnv.useDatabase("iceberg_db");
+
+        // 批量读取Iceberg表
+        System.out.println("开始批量读取Iceberg表");
+        TableResult tableResult = tEnv.executeSql("select * from flink_sql_hadoop_iceberg_table");
+        tableResult.print();
+
+        // 实时读取Iceberg表
+        System.out.println("开始实时读取Iceberg表");
+        TableResult tableStreamResult = tEnv.executeSql("select * from flink_sql_hadoop_iceberg_table /*+ OPTIONS('streaming'='true', 'monitor-interval'='2s')*/");  // OPTIONS可以指定snapshot 'start-snapshot-id'='3821550127947089987'
+        tableStreamResult.print();
+
+    }
+}
--- Iceberg/src/main/scala/top/shmily_qjj/iceberg/table/maintenance/SparkIcebergTableMaintenance.scala
+++ Iceberg/src/main/scala/top/shmily_qjj/iceberg/table/maintenance/SparkIcebergTableMaintenance.scala
@@ -1,11 +1,17 @@
 package top.shmily_qjj.iceberg.table.maintenance
 
-import org.apache.iceberg.ManifestFile
+import com.google.common.collect.ImmutableMap
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.iceberg.{ManifestFile, hadoop, hive}
 import org.apache.spark.sql.SparkSession
 import org.apache.iceberg.catalog.TableIdentifier
 import org.apache.iceberg.expressions.{Expressions, Predicate}
+import org.apache.iceberg.spark.actions.DeleteOrphanFilesSparkAction
+import org.apache.iceberg.hadoop.HadoopCatalog
 import org.apache.iceberg.hive.HiveCatalog
 import org.apache.iceberg.spark.actions.SparkActions
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * Iceberg Table Maintenance Using Spark
@@ -13,13 +19,26 @@ import org.apache.iceberg.spark.actions.SparkActions
  */
 object SparkIcebergTableMaintenance {
   def main(args: Array[String]): Unit = {
-    val spark: SparkSession = SparkSession.builder.getOrCreate()
+    val spark: SparkSession = SparkSession.builder.master("local").appName("MaintenanceIcebergTables").getOrCreate()
+    val conf = new Configuration
+    val hadoopResources = "E:\\CDH-Conf\\core-site.xml,E:\\CDH-Conf\\hdfs-site.xml"
+    for (sourceFile <- hadoopResources.split(",")) {
+      conf.addResource(new Path(sourceFile))
+    }
 
     // 清理过期快照
-    expireSnapshots(spark, 5L)
-    removeOrphanFiles(spark)
-    compactDataFiles(spark, 128L)
-    rewriteManifests(spark, 10L)
+    expireSnapshots(spark, 5L, conf)
+//    removeOrphanFiles(spark, conf)
+//    compactDataFiles(spark, 128L, conf)
+//    rewriteManifests(spark, 10L, conf)
+  }
+
+  def getHadoopCatalog(conf: Configuration, warehousePath: String): HadoopCatalog = new HadoopCatalog(conf, warehousePath)
+  def getHiveCatalog(conf: Configuration, metastoreUris: String, warehousePath: String): HiveCatalog = {
+    val catalog = new HiveCatalog()
+    catalog.setConf(conf)
+    catalog.initialize("HiveCatalog", ImmutableMap.of("uri", metastoreUris, "warehouse", warehousePath, "clients", "5"))
+    catalog
   }
 
   /**
@@ -27,12 +46,11 @@ object SparkIcebergTableMaintenance {
    * @param spark sparkSession
    * @param snapRetainMinutes 快照保留时间
    */
-  def expireSnapshots(spark: SparkSession, snapRetainMinutes: Long): Unit = {
+  def expireSnapshots(spark: SparkSession, snapRetainMinutes: Long, conf: Configuration): Unit = {
     val tsToExpire = System.currentTimeMillis() - (1000 * 60 * snapRetainMinutes)  //设置清理时间
-    val catalog = new HiveCatalog()
-    catalog.setConf(spark.sessionState.newHadoopConf)
-    val table = catalog.loadTable(TableIdentifier.of("iceberg_db", "nt_order_detail"))
-    val result = SparkActions.get().expireSnapshots(table).expireOlderThan(tsToExpire).execute()
+    val catalog = getHadoopCatalog(conf, "hdfs://nameservice/user/iceberg/warehouse")
+    val table = catalog.loadTable(TableIdentifier.of("iceberg_db", "flink_hadoop_iceberg_table"))
+    val result = SparkActions.get(spark).expireSnapshots(table).expireOlderThan(tsToExpire).execute()
     println(s"#ExpireSnapshots Delete Data Files Count:${result.deletedDataFilesCount()}")
     println(s"#ExpireSnapshots Delete Manifest Files Count:${result.deletedManifestsCount()}")
   }
@@ -42,28 +60,26 @@ object SparkIcebergTableMaintenance {
    * In Spark and other distributed processing engines, task or job failures can leave files that are not referenced by table metadata, and in some cases normal snapshot expiration may not be able to determine a file is no longer needed and delete it.
    * @param spark sparkSession
    */
-  def removeOrphanFiles(spark: SparkSession): Unit = {
-    val catalog = new HiveCatalog()
-    catalog.setConf(spark.sessionState.newHadoopConf)
-    val table = catalog.loadTable(TableIdentifier.of("iceberg_db", "nt_order_detail"))
+  def removeOrphanFiles(spark: SparkSession, conf:Configuration): Unit = {
+    val catalog = getHadoopCatalog(conf, "hdfs://nameservice/user/iceberg/warehouse")
+    val table = catalog.loadTable(TableIdentifier.of("iceberg_db", "flink_hadoop_iceberg_table"))
     val result = SparkActions.get().deleteOrphanFiles(table).execute()
     result.orphanFileLocations().forEach(println)
   }
 
   /**
-   * 并行合并数据文件
+   * 并行合并数据文件 合并小文件
    * @param spark sparkSession
    * @param targetFileSizeMb 合并后的目标文件大小
    * Iceberg tracks each data file in a table. More data files leads to more metadata stored in manifest files, and small data files causes an unnecessary amount of metadata and less efficient queries from file open costs.
    */
-  def compactDataFiles(spark: SparkSession, targetFileSizeMb: Long): Unit = {
-    val catalog = new HiveCatalog()
-    catalog.setConf(spark.sessionState.newHadoopConf)
-    val table = catalog.loadTable(TableIdentifier.of("iceberg_db", "nt_order_detail"))
+  def compactDataFiles(spark: SparkSession, targetFileSizeMb: Long, conf: Configuration): Unit = {
+    val catalog = getHadoopCatalog(conf, "hdfs://nameservice/user/iceberg/warehouse")
+    val table = catalog.loadTable(TableIdentifier.of("iceberg_db", "flink_hadoop_iceberg_table"))
     val result = SparkActions
       .get()
       .rewriteDataFiles(table)
-      .filter(Expressions.equal("date", "2020-08-18"))
+//      .filter(Expressions.equal("date", "2020-08-18"))
       .option("target-file-size-bytes", (targetFileSizeMb * 1024 * 1024).toString)
       .execute()
     result.rewriteResults().forEach(println)
@@ -78,10 +94,9 @@ object SparkIcebergTableMaintenance {
    * When a table’s write pattern doesn’t align with the query pattern, metadata can be rewritten to re-group data files into manifests using rewriteManifests or the rewriteManifests action (for parallel rewrites using Spark).
    * This example rewrites small manifests and groups data files by the first partition field.
    */
-  def rewriteManifests(spark: SparkSession, rewriteThresholdMb: Long): Unit = {
-    val catalog = new HiveCatalog()
-    catalog.setConf(spark.sessionState.newHadoopConf)
-    val table = catalog.loadTable(TableIdentifier.of("iceberg_db", "nt_order_detail"))
+  def rewriteManifests(spark: SparkSession, rewriteThresholdMb: Long, conf: Configuration): Unit = {
+    val catalog = getHadoopCatalog(conf, "hdfs://nameservice/user/iceberg/warehouse")
+    val table = catalog.loadTable(TableIdentifier.of("iceberg_db", "flink_hadoop_iceberg_table"))
     val rewritePredicate: java.util.function.Predicate[ManifestFile] = ((file: ManifestFile) => file.length() < rewriteThresholdMb * 1024 * 1024)
     val result = SparkActions
       .get()
