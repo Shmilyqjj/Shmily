6dde502dbb434d99cf487bc16db845ce225419eb
shmilyqjj <710552907@qq.com>
2023-07-12 15:45:50 +0800

    add redis java client utils

---
+++ Redis/src/redis/client/JedisClient.java
@@ -0,0 +1,56 @@
+package redis.client;
+
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * @author shmily
+ */
+public class JedisClient {
+    private final Jedis jedis;
+
+    public JedisClient(String host, int port, String password) {
+        this.jedis = new Jedis(host, port);
+        if (password != null && password.length() != 0) {
+            this.jedis.auth(password);
+        }
+    }
+
+    public void set(String key, String value) {
+        jedis.set(key, value);
+    }
+
+    public String get(String key) {
+        return jedis.get(key);
+    }
+
+    public boolean exists(String key) {
+        return jedis.exists(key);
+    }
+
+    public void delete(String key) {
+        jedis.del(key);
+    }
+
+    public void close() {
+        jedis.close();
+    }
+
+    // test
+    public static void main(String[] args) {
+        JedisClient redisUtils = new JedisClient("localhost", 6379, "123456");
+
+        redisUtils.set("key", "value");
+        String value = redisUtils.get("key");
+        System.out.println("Value: " + value);
+
+        boolean exists = redisUtils.exists("key");
+        System.out.println("Exists: " + exists);
+
+        redisUtils.delete("key");
+        exists = redisUtils.exists("key");
+        System.out.println("Exists: " + exists);
+
+        redisUtils.close();
+    }
+}
---
+++ Redis/src/redis/client/LettuceClient.java
@@ -0,0 +1,71 @@
+package redis.client;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * @author shmily
+ */
+public class LettuceClient {
+    private final RedisClient redisClient;
+    private final StatefulRedisConnection<String, String> connection;
+    private final RedisCommands<String, String> redisCommands;
+
+    public LettuceClient(String host, int port, CharSequence password, int timeoutSeconds){
+        RedisURI redisUri = RedisURI.builder()
+                .withHost(host)
+                .withPort(port)
+                .withPassword(password)
+                .withTimeout(Duration.of(timeoutSeconds, ChronoUnit.SECONDS))
+                .build();
+        this.redisClient = RedisClient.create(redisUri);
+        this.connection = redisClient.connect();
+        this.redisCommands = connection.sync();
+    }
+
+
+    public void set(String key, String value) {
+        redisCommands.set(key, value);
+    }
+
+    public String get(String key) {
+        return redisCommands.get(key);
+    }
+
+    public boolean exists(String key) {
+        return redisCommands.exists(key) != 0;
+    }
+
+    public void delete(String key) {
+        redisCommands.del(key);
+    }
+
+    public void close() {
+        connection.close();
+        redisClient.shutdown();
+    }
+
+
+    public static void main(String[] args) {
+        LettuceClient lettuceClient = new LettuceClient("localhost", 6379, "123456", 10);
+
+        lettuceClient.set("key", "value");
+        String value = lettuceClient.get("key");
+        System.out.println("Value: " + value);
+
+        boolean exists = lettuceClient.exists("key");
+        System.out.println("Exists: " + exists);
+
+        lettuceClient.delete("key");
+        exists = lettuceClient.exists("key");
+        System.out.println("Exists: " + exists);
+
+        lettuceClient.close();
+    }
+
+}
