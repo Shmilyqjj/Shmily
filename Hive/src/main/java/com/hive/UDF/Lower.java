2de669f1165f00a3f21bd3926ef201bbf7ecf88f
shmilyqjj <710552907@qq.com>
2023-07-31 19:41:29 +0800

    udf

--- Hive/src/main/java/com/hive/UDF/Lower.java
+++ Hive/src/main/java/com/hive/UDF/Lower.java
@@ -1,7 +1,19 @@
 package com.hive.UDF;
 
+import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
+/**
+ * Usage:
+ * add jar /opt/modules/hive/auxlib/Hive-1.0-SNAPSHOT.jar;
+ * CREATE TEMPORARY FUNCTION lower_case AS 'com.hive.UDF.Lower';
+ * select lower_case('Haha');
+ * @author shmily
+ */
+@Description(name = "lower",
+        value = "lower(string_value) - Returns the lower string.",
+        extended = "Example:\n"
+                + "  > SELECT lower('HAHA123');\n" + "  haha123")
 public class Lower extends UDF {
 
     public String evaluate (final String s) {
