e1b1c4200a64c9b72fa1bf0ae486521b8c2ca09c
shmilyqjj <710552907@qq.com>
2024-07-31 12:07:01 +0800

    test scala

---
+++ LearnScala/src/main/scala/base/Test.scala
@@ -0,0 +1,13 @@
+package base
+
+/**
+ * @Description: 测试Scala代码
+ * @CreateTime: 2024/7/29 下午5:28
+ * @author Shmily
+ */
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    println(this.getClass.getName.split('$')(0))
+  }
+}
