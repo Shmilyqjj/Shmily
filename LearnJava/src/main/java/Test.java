c5d53b982d9832d4d2f0565aecb92299056fc54f
shmilyqjj <710552907@qq.com>
2024-12-05 16:47:36 +0800

    anno

--- LearnJava/src/main/java/Test.java
+++ LearnJava/src/main/java/Test.java
@@ -1,9 +1,11 @@
+import common.MyAnnotation;
 
-
+@MyAnnotation
 public class Test {
+
+    @MyAnnotation
     public static void main(String[] args) throws InterruptedException {
-
-
+        System.out.println(1);
     }
 }
 
