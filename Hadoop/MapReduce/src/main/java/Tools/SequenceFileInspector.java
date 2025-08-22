1bb0d3b5d19235174a10c179c2c95a33f07aa87a
shmilyqjj <710552907@qq.com>
2025-08-22 11:45:51 +0800

    sequenceFile解析

--- Hadoop/MapReduce/src/main/java/Tools/SequenceFileInspector.java
+++ Hadoop/MapReduce/src/main/java/Tools/SequenceFileInspector.java
@@ -4,6 +4,7 @@ package Tools;
  * Description: SequenceFileInspector
  * CreateTime: 2025/8/1 14:46
  * Author Shmily
+ * 运行时指定环境变量 LD_LIBRARY_PATH=/opt/modules/hadoop/lib/native
  */
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -19,7 +20,7 @@ public class SequenceFileInspector {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(conf);
-        Path path = new Path("/home/shmily/Downloads/Temp/part-00000-527d539c-75b0-44cc-885f-dc4f3e2cdb4d-c000");
+        Path path = new Path("/home/shmily/Downloads/Temp/part-00000-e8c4734c-30d6-4062-93da-7cbb42d76de4-c000");
 
         try (Reader reader = new SequenceFile.Reader(fs, path, conf)) {
             System.out.println("Compression Type: " + reader.getCompressionType());
